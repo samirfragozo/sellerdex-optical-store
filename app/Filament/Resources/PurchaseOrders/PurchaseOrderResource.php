e04f92608c80c89181a1a61b3d07bbbcf031a425
Samir Fragozo <samirfragozob@gmail.com>
2026-06-28 19:08:17 -0500

    refactor(filament): move suppliers into the Compras cluster with purchase orders

--- app/Filament/Resources/PurchaseOrders/PurchaseOrderResource.php
+++ app/Filament/Resources/PurchaseOrders/PurchaseOrderResource.php
@@ -25,7 +25,7 @@ class PurchaseOrderResource extends Resource
 
     protected static ?string $cluster = ComprasCluster::class;
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 2;
 
     public static function getModelLabel(): string
     {
